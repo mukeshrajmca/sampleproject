c52c387d8189e2b070b2a6f15ca9161b36f7bd4b
mukesh <mukeshrajmca@gmail.com>
2018-05-20 23:18:20 +0530

    This is my first commit for hello-world

---
+++ sampleproject1/src/sampleproject1/helloWorld.java
@@ -0,0 +1,11 @@
+package sampleproject1;
+
+public class helloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hello world from sample project");
+	}
+
+}
